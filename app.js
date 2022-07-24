66b51d149a047e9739b883125ed2b9d06e99adff
B10756052 <B10756052@gmail.com>
2022-07-24 14:12:07 +0800

    gh-pages commit 2

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-let q_header = document.querySelector('header');
-let q_a = document.querySelectorAll('header nav ul li a');
+let q_header = document.querySelector("header");
+let q_a = document.querySelectorAll("header nav ul li a");
 
-window.addEventListener('scroll', () => {
-    if (window.pageYOffset != 0) {
-        q_header.style.backgroundColor = 'rgba(0,0,0,0.3)';
-        q_header.style.color = "white";
-        q_a.forEach((a) => {
-            a.style.color = "white"
-        });
-
-    } else {
-        q_header.style = "";
-        q_a.forEach((a) => { a.style = "" });
-    }
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset != 0) {
+    q_header.style.backgroundColor = "rgba(0,0,0,0.3)";
+    q_header.style.color = "white";
+    q_a.forEach((a) => {
+      a.style.color = "white";
+    });
+  } else {
+    q_header.style = "";
+    q_a.forEach((a) => {
+      a.style = "";
+    });
+  }
 });
+
+console.log("志j好帥");
