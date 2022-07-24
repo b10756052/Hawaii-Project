43151421fd1841945d136f348a86996904ab1a7c
B10756052 <B10756052@gmail.com>
2022-07-24 14:00:32 +0800

    當初跟著課程完成的夏威夷project

---
+++ app.js
@@ -0,0 +1,16 @@
+let q_header = document.querySelector('header');
+let q_a = document.querySelectorAll('header nav ul li a');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset != 0) {
+        q_header.style.backgroundColor = 'rgba(0,0,0,0.3)';
+        q_header.style.color = "white";
+        q_a.forEach((a) => {
+            a.style.color = "white"
+        });
+
+    } else {
+        q_header.style = "";
+        q_a.forEach((a) => { a.style = "" });
+    }
+});
